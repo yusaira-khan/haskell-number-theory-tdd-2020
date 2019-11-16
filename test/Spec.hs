ac104f97f31126a342b9be6f6442f0502810fc83
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-11-15 21:16:34 -0500

    from book

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,8 @@
 -- module Spec where
 import Test.Hspec
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Addition" $ do
+   it "1 + 1 is greater than 1" $ do
+     (1 + 1) > 1 `shouldBe` True it "2 + 2 is equal to 4" $ do
+       2 + 2 `shouldBe` 4
