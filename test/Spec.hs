d0a26e5ad75577c7d59b2891417abf2e63f121e2
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-11-15 21:22:30 -0500

    compiles with spaces

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,9 @@
 -- module Spec where
 import Test.Hspec
 main :: IO ()
-main = hspec $ do {
-  describe "Addition" $ do {
-   it "1 + 1 is greater than 1" $ do {
-     (1 + 1) > 1 `shouldBe` True};
-   it "2 + 2 is equal to 4" $ do {
-     2 + 2 `shouldBe` 4}}}
+main = hspec $ do 
+  describe "Addition" $ do 
+   it "1 + 1 is greater than 1" $ do 
+     (1 + 1) > 1 `shouldBe` True
+   it "2 + 2 is equal to 4" $ do 
+     2 + 2 `shouldBe` 4
