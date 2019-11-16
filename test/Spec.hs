1c3e23a8c2e416122cb715b802fe5a9cd54cbb12
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-11-15 21:40:23 -0500

    added print

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,16 @@
 module Main where
 import Test.Hspec
 import Test.QuickCheck
+import Lib as L
 
 main :: IO ()
-main = hspec $ do
-  describe "Addition" $ do
-   it "1 + 1 is greater than 1" $ do
-     (1 + 1) > 1 `shouldBe` True
-   it "2 + 2 is equal to 4" $ do
-     2 + 2 `shouldBe` 4
-   it "x + 1 is always greater than x" $ do
-     property $ \x -> x + 1 > (x :: Int)
+main = do
+  hspec $ do
+    describe "Addition" $ do
+      it "1 + 1 is greater than 1" $ do
+        (1 + 1) > 1 `shouldBe` True
+      it "2 + 2 is equal to 4" $ do
+        2 + 2 `shouldBe` 4
+      it "x + 1 is always greater than x" $ do
+        property $ \x -> x + 1 > (x :: Int)
+  L.someFunc
