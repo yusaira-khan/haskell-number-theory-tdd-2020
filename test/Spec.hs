6513f167e466fd05a8076e0e62922b82134c0f59
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-11-16 00:32:00 -0500

    wont compile undefined

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,14 +4,20 @@ import Test.QuickCheck
 import Lib as L
 import SparseBinary as SB
 
+emptySparse :: SpecWith (Arg Int)
+emptySparse = it "Empty" undefined
+exampleTest :: IO()
+exampleTest = do
+   hspec $ do
+     describe "Addition" $ do
+       it "1 + 1 is greater than 1" $ do
+         (1 + 1) > 1 `shouldBe` True
+       it "2 + 2 is equal to 4" $ do
+         2 + 2 `shouldBe` 4
+       it "x + 1 is always greater than x" $ do
+         property $ \x -> x + 1 > (x :: Int)
+   L.someFunc
+
 main :: IO ()
 main = do
-  hspec $ do
-    describe "Addition" $ do
-      it "1 + 1 is greater than 1" $ do
-        (1 + 1) > 1 `shouldBe` True
-      it "2 + 2 is equal to 4" $ do
-        2 + 2 `shouldBe` 4
-      it "x + 1 is always greater than x" $ do
-        property $ \x -> x + 1 > (x :: Int)
-  L.someFunc
+  exampleTest
