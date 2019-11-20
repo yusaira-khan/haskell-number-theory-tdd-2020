4bf64cd603e0be5c702cd90c83552d894bf3aeeb
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-11-20 18:37:35 -0500

    refactor sb to enum to use guards

--- src/SparseBinary.hs
+++ src/SparseBinary.hs
@@ -1,3 +1,6 @@
 module SparseBinary(toEnum') where
 toEnum' :: Int -> [Int]
-toEnum' num = if (num == 0) then [] else if num > 2 then (toEnum' (num -2))++[2] else [num]
+toEnum' 0 = []
+toEnum' num = if num > 2
+  then (toEnum' (num -2))++[2]
+  else [num]
