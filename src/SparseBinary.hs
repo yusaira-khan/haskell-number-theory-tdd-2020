c83ba4f7b0ac2b7d8fb37f3675c57cddcb691d96
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-11-20 18:43:53 -0500

    4 passes

--- src/SparseBinary.hs
+++ src/SparseBinary.hs
@@ -1,6 +1,11 @@
 module SparseBinary(toEnum') where
 toEnum' :: Int -> [Int]
 toEnum' 0 = []
-toEnum' num = if num > 2
-  then (toEnum' (num -2))++[2]
-  else [num]
+toEnum' num =
+  if num > 4
+  then toEnum' (num -4)++[4]
+  else if num == 4
+    then [num]
+    else if num > 2
+      then toEnum' (num -2)++[2]
+      else [num]
