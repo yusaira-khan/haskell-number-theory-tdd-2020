4674d44c92bcb328408c442ced8f50a8746225ae
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-11-21 16:47:58 -0500

    restore behaviour in refactor

--- src/SparseTernary.hs
+++ src/SparseTernary.hs
@@ -1,10 +1,10 @@
 module SparseTernary(toEnum') where
-getPow3 num = 3
+getPow3 num = if num < 3 then 1 else 3
 toEnum' :: Int -> [Int]
 toEnum' 0 = []
 toEnum' num =
   let pow3 = getPow3 num
   in let pow3Mul = num `quot` pow3
-    in if (num`mod`pow3==0)
-      then [num]
-      else (toEnum' (num -pow3Mul))++[pow3Mul]
+  in if (num`mod`pow3==0)
+    then [num]
+    else (toEnum' (num -pow3))++[pow3]
