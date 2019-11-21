092823344ad320fd1be4b08c88f18246de4b1dbb
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-11-21 18:25:22 -0500

    refactor getting power of 3

--- src/SparseTernary.hs
+++ src/SparseTernary.hs
@@ -1,10 +1,19 @@
 module SparseTernary(toEnum') where
-getPow3 :: Int-> Int
-getPow3 num = if num >=9 then 9 else if num < 3 then 1 else 3
+
+getLargestPow3Between :: Int -> Int -> Int
+getLargestPow3Between pow3 high =
+  case compare pow3 high of
+    GT -> pow3 `quot` 3
+    EQ -> pow3
+    LT -> getLargestPow3Between (pow3*3) high
+
+getLargestPow3LessThan :: Int-> Int
+getLargestPow3LessThan = getLargestPow3Between 1
+
 toEnum' :: Int -> [Int]
 toEnum' 0 = []
 toEnum' num =
-  let pow3 = getPow3 num
+  let pow3 =  getLargestPow3LessThan num
       pow3Mul = (num `quot` pow3)*pow3
       rest = num `rem` pow3
   in if (num`mod`pow3==0)
