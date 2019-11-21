40134dd8a12e046fa330953607c75635f582a300
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-11-20 22:54:32 -0500

    make 6 pass

--- src/SparseTernary.hs
+++ src/SparseTernary.hs
@@ -1,4 +1,7 @@
 module SparseTernary(toEnum') where
 toEnum' :: Int -> [Int]
 toEnum' 0 = []
-toEnum' num = if num <= 3 then [num] else (toEnum' (num -3))++[3]
+toEnum' num =
+  if ((num <= 3) || (num`mod`3==0))
+  then [num]
+  else (toEnum' (num -3))++[3]
