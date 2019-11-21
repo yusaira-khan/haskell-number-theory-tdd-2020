9f919390caf826db84a98ef28df93408cee10d8a
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-11-21 16:42:44 -0500

    attempt to make 6 work

--- src/SparseTernary.hs
+++ src/SparseTernary.hs
@@ -4,6 +4,7 @@ toEnum' :: Int -> [Int]
 toEnum' 0 = []
 toEnum' num =
   let pow3 = getPow3 num
-  in if ((num <= pow3) || (num`mod`pow3==0))
-    then [num]
-    else (toEnum' (num -pow3))++[pow3]
+  in let pow3Mul = num `quot` pow3
+    in if (num`mod`pow3==0)
+      then [num]
+      else (toEnum' (num -pow3Mul))++[pow3Mul]
