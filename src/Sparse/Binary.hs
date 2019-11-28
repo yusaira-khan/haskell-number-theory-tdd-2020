b4ffe51c5e3ad3d80caf57f0bacfe560685c81f3
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-11-28 11:22:30 -0500

    passing 0 one

--- src/Sparse/Binary.hs
+++ src/Sparse/Binary.hs
@@ -5,12 +5,17 @@ import Sparse.Helper as H
 toEnum' :: Int -> [Int]
 toEnum' = H.toEnumInBase 2
 
-newtype SBinary = SBinary {sBinary :: [Int]}
 
+eqlist :: [Int] -> [Int] -> Bool
+eqlist [] [] = True
+eqlist [] _ = False
+
+
+newtype SBinary = SBinary {sBinary :: [Int]}
 instance Show SBinary where
   show = H.show' sBinary (H.showReprWBase 2.sBinary)
 instance Enum SBinary where
   toEnum d = SBinary $ toEnum' d
   fromEnum sb = H.fromEnum' $ sBinary sb
 instance Eq SBinary where
-  (==) sb1 sb2 = True
+  (==) sb1 sb2 = eqlist (sBinary sb1) (sBinary sb2)
