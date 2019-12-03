44de94f1a7a9407e8fe1559c491cb1698144cbbf
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-12-03 00:13:50 -0500

    thousand name works

--- test/NumberNames.hs
+++ test/NumberNames.hs
@@ -10,7 +10,18 @@ nameTys num =
   in namety ++ (nameRest numOne)
 nameRest 0 = ""
 nameRest num = " " ++ (names num)
-nameHundred num = undefined
+nameThousand num =
+  let
+    (numThousand,numHundred) = quotRem num 1000
+    nameT = names numThousand
+    nameR = nameRest numHundred
+  in nameT ++ " Thousand" ++nameR
+nameHundred num =
+  let
+    (numHundred,numTens) = quotRem num 100
+    nameH = names numHundred
+    nameR = nameRest numTens
+  in nameH ++ " Hundred" ++nameR
 names :: Int -> String
 names num =
   if 0 <= num && num< 10
@@ -19,8 +30,10 @@ names num =
   then nameTeens !! (num - 10)
   else if 20 <= num && num < 100
   then nameTys num
-  else if 100 <= num && num < 100
-  then nameTys num
+  else if 100 <= num && num < 1000
+  then nameHundred num
+  else if 1000 <= num && num < 1000000
+  then nameThousand num
   else undefined
 
 checkNames :: (Int,String) -> SpecWith ()
@@ -33,4 +46,7 @@ spec = describe "Testing number names" $ do
   checkNames (20,"Twenty")
   checkNames (21,"Twenty One")
   checkNames (57,"Fifty Seven")
-  -- checkNames (164,"One Hundred Sixty Four")
+  checkNames (164,"One Hundred Sixty Four")
+  checkNames (100,"One Hundred")
+  checkNames (12000,"Twelve Thousand")
+  checkNames (918703,"Nine Hundred Eighteen Thousand Seven Hundred Three")
