5086e4f51f600bc3306e9abda731f0dd59d601d2
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-12-03 00:01:06 -0500

    add twenty name

--- test/NumberNames.hs
+++ test/NumberNames.hs
@@ -1,13 +1,26 @@
 module NumberNames (names,spec)where
 import Test.Hspec
 nameOnes = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]
-nameTens = ["Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"]
+nameTeens = ["Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"]
+nameTyList = ["Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"]
+nameTys num =
+  let
+    (numTy,numOne) = quotRem num 10
+    namety = nameTyList !! ( numTy - 2)
+  in namety ++ (nameRest numOne)
+nameRest 0 = ""
+nameRest num = " " ++ (names num)
+nameHundred num = undefined
 names :: Int -> String
 names num =
   if 0 <= num && num< 10
   then nameOnes !! num
-  else if 10 <= num && num <= 20
-  then nameTens !! (num - 10)
+  else if 10 <= num && num < 20
+  then nameTeens !! (num - 10)
+  else if 20 <= num && num < 100
+  then nameTys num
+  else if 100 <= num && num < 100
+  then nameTys num
   else undefined
 
 checkNames :: (Int,String) -> SpecWith ()
@@ -17,3 +30,7 @@ spec :: Spec
 spec = describe "Testing number names" $ do
   checkNames (0,"Zero")
   checkNames (11,"Eleven")
+  checkNames (20,"Twenty")
+  checkNames (21,"Twenty One")
+  checkNames (57,"Fifty Seven")
+  -- checkNames (164,"One Hundred Sixty Four")
