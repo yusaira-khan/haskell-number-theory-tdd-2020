b2bfe5dd1560fb04a51b94e0a65b9dcbc523f2db
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2019-12-03 21:18:37 -0500

    refactor name generation

--- test/NumberNames.hs
+++ test/NumberNames.hs
@@ -1,39 +1,30 @@
 module NumberNames (names,spec)where
 import Test.Hspec
-nameOnes = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]
-nameTeens = ["Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"]
-nameTyList = ["Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"]
-nameTys num =
-  let
-    (numTy,numOne) = quotRem num 10
-    namety = nameTyList !! ( numTy - 2)
-  in namety ++ (nameRest numOne)
+ones = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]
+teens = ["Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"]
+tys = ["Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"]
+nameList list accessorFun num = list !! (accessorFun num)
 nameRest 0 = ""
 nameRest num = " " ++ (names num)
-nameThousand num =
+nameGreaterThan unit nameExt num=
   let
-    (numThousand,numHundred) = quotRem num 1000
-    nameT = names numThousand
-    nameR = nameRest numHundred
-  in nameT ++ " Thousand" ++nameR
-nameHundred num =
-  let
-    (numHundred,numTens) = quotRem num 100
-    nameH = names numHundred
-    nameR = nameRest numTens
-  in nameH ++ " Hundred" ++nameR
+    (numGreater,numRest) = quotRem num unit
+    nameGreater = (names numGreater) ++ " " ++  nameExt
+    nameR = nameRest numRest
+  in nameGreater ++ nameR
+numCond (rangeStart, rangeEnd) num = rangeStart <= num && num < rangeEnd
 names :: Int -> String
 names num =
-  if 0 <= num && num< 10
-  then nameOnes !! num
-  else if 10 <= num && num < 20
-  then nameTeens !! (num - 10)
-  else if 20 <= num && num < 100
-  then nameTys num
-  else if 100 <= num && num < 1000
-  then nameHundred num
-  else if 1000 <= num && num < 1000000
-  then nameThousand num
+  if numCond (0,10) num
+  then nameList ones id num
+  else if numCond (10,20) num
+  then nameList teens (\n -> (n - 10)) num
+  else if numCond (20,100) num
+  then (nameList tys (\n -> (n `quot` 10)-2) num) ++ nameRest (num `rem` 10)
+  else if numCond (100,1000) num
+  then nameGreaterThan 100 "Hundred" num
+  else if numCond (1000,1000000) num
+  then nameGreaterThan 1000 "Thousand" num
   else undefined
 
 checkNames :: (Int,String) -> SpecWith ()
