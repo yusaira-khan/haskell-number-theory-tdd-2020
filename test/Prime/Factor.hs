743142d7af99263ea63d9da993fc6af5475b81ce
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2020-04-26 18:42:56 -0400

    change behavior for 2

--- test/Prime/Factor.hs
+++ test/Prime/Factor.hs
@@ -1,12 +1,15 @@
 module Prime.Factor (spec)where
 import Test.Hspec
 
-factor :: Int -> [Int]
-factor _ = []
+primeFactorsOf :: Int -> [Int]
+primeFactorsOf n =
+  let factors = if n >1 then [2] else [] 
+  in factors
 
 checkFactors :: Int -> [Int] -> SpecWith ()
-checkFactors num factors = it ((show num)++" -> "++(show factors) )$ (factor num) `shouldBe` factors
+checkFactors num factors = it ((show num)++" -> "++(show factors) )$ (primeFactorsOf num) `shouldBe` factors
 
 spec :: Spec
 spec = describe "factors" $ do
   checkFactors 1 []
+  checkFactors 2 [2]
