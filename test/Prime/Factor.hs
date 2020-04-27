1aa29fd52bd68efc71b16c302dc8df087b97c372
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2020-04-26 20:12:53 -0400

    refactor to do 1 first

--- test/Prime/Factor.hs
+++ test/Prime/Factor.hs
@@ -3,12 +3,12 @@ import Test.Hspec
 
 primeFactorsStarting :: Int -> Int -> [Int]
 primeFactorsStarting d n =
-  if n > 1
-  then
+  if n <= 1
+  then []
+  else
     if (n `mod` d)==0
     then d:primeFactorsStarting d (n `div` d)
     else primeFactorsStarting (d+1) n
-  else []
 primeFactorsOf :: Int -> [Int]
 primeFactorsOf = primeFactorsStarting 2
 checkFactors :: Int -> [Int] -> SpecWith ()
@@ -25,3 +25,8 @@ spec = describe "factors" $ do
   checkFactors 7 [7]
   checkFactors 8 [2,2,2]
   checkFactors 9 [3,3]
+  checkFactors 10 [2,5]
+  checkFactors 11 [11]
+  checkFactors 12 [2,2,3]
+  checkFactors 45 [3,3,5]
+  checkFactors 175 [5,5,7]
